d50dd4bb389a3d8043d1d8e94b9b41f10ee898e2
Krishna960333 <80269720+Krishna960333@users.noreply.github.com>
2021-05-27 22:58:05 +0530

    Update deploy.ps1

--- docs/reference/contoso/armTemplates/deploy.ps1
+++ docs/reference/contoso/armTemplates/deploy.ps1
@@ -5,36 +5,34 @@ Connect-AzAccount
 
 $user = Get-AzADUser -UserPrincipalName (Get-AzContext).Account
 
-New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $user.Id
-remove-azroleassignment -scope / -objectId 39a368c8-d73a-4c59-9ff7-97ac13e8150a -roledefinitionname Owner
+New-AzRoleAssignment -Scope '/Tenant Root Group/TAF-FMO' -RoleDefinitionName 'Owner' -ObjectId $user.Id
+remove-azroleassignment -scope /Tenant Root Group/TAF-FMO -objectId a658f601-cfd4-4e7e-84f0-1ecf1a850dde -roledefinitionname Owner
 
-new-azroleassignment -scope / -objectId 39a368c8-d73a-4c59-9ff7-97ac13e8150a -roledefinitionname Owner
+new-azroleassignment -scope /Tenant Root Group/TAF-FMO -objectId a658f601-cfd4-4e7e-84f0-1ecf1a850dde -roledefinitionname Owner
 
 
 New-AzManagementGroupDeployment `
-  -Name old-2-mg `
-  -Location "uksouth" `
-  -ManagementGroupId "old-3-mg" `
-  -TemplateUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg.json" `
-  -TemplateParameterUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json" `
+  -Name TAF-FMO-Deployment `
+  -Location "West Europe" `
+  -ManagementGroupId "TAF-FMO" `
+  -TemplateUri "https://github.com/sanjaykrsinghgit/rep_lz/blob/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-parameters.json" `
+  -TemplateParameterUri "https://github.com/sanjaykrsinghgit/rep_lz/blob/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json" `
   -WhatIf
 
-  New-AzManagementGroupDeployment `
-  -Name old-3-mg-deployment `
-  -Location "uksouth" `
-  -ManagementGroupId "old-3-mg" `
-  -TemplateUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg.json" `
-  -TemplateParameterUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json"
+New-AzManagementGroupDeployment `
+  -Name TAF-FMO-Deploy `
+  -Location "West Europe" `
+  -ManagementGroupId "TAF-FMO" `
+  -TemplateUri "https://github.com/sanjaykrsinghgit/rep_lz/blob/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-parameters.json" `
+  -TemplateParameterUri "https://github.com/sanjaykrsinghgit/rep_lz/blob/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json" `
 
 
 
-  New-AzManagementGroupSubscription -GroupName 'Enterprise-Scale' -SubscriptionId '035f3891-2d42-41cc-8e7d-71ec904d4980'
-  Remove-AzManagementGroupSubscription -GroupName 'old-3-mg-management' -SubscriptionId '035f3891-2d42-41cc-8e7d-71ec904d4980'
+  New-AzManagementGroupSubscription -GroupName 'Enterprise-Scale' -SubscriptionId 'b4fdb68b-dfb2-4a38-bb44-8c47839f6ab7'
 
-  New-AzManagementGroupSubscription -GroupName 'Enterprise-Scale' -SubscriptionId '1b67375a-eee0-4a74-89aa-9e8d04483fbb'
-  Remove-AzManagementGroupSubscription -GroupName 'old-3-mg-corp' -SubscriptionId '1b67375a-eee0-4a74-89aa-9e8d04483fbb'
+  New-AzManagementGroupSubscription -GroupName 'Enterprise-Scale' -SubscriptionId '1abcdfd7-3630-4c35-a81c-e6bae3e241fa'
   
-  $toplevelgroup = Get-AzManagementGroup -GroupName 'old-3-mg' -Expand -Recurse
+  $toplevelgroup = Get-AzManagementGroup -GroupName 'TAF-FMO' -Expand -Recurse
   $children = $toplevelgroup.Children
   $grandchildren = $toplevelgroup.Children.Children
   foreach ($grandchild in $grandchildren) {
